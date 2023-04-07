f80336a1c3d655cdebee3e0c5694cfda568c2179
Nagarjuna <kollinagarjunareddy123@gmail.com>
2023-04-07 16:35:50 +0530

    heykolli commit 2nd time

--- Test.java
+++ Test.java
@@ -4,7 +4,7 @@ public class Test {
 	private void mmain() {
 		System.out.println("hello nagarjuna");
 		System.out.println("hello nagarjuna");
-		System.out.println("hello nagarjuna");
+		
 		System.out.println("hello nagarjuna");
 	}
 }
